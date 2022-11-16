29cd0f67eb7e8bd1d4e8c8984c6665619f31f932
S1riyS <kirill.ankudinov.94@mail.ru>
2022-11-16 21:26:16 +0500

    FEATURE: added html and css files

--- csgamble/apps/cases/views.py
+++ csgamble/apps/cases/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Case
 
 
 def index(request):
     return HttpResponse('<h1>CASES</h1>')
 
+
 def case(request, case_name):
-    return HttpResponse(f'<h1>SPECIFIC CASE - {case_name}</h1>')
+    case = get_object_or_404(Case, name=case_name)
+    context = {
+        'case': case
+    }
+    return render(request, 'cases/case.html', context)
